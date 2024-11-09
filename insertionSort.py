f5262ef206e202d67f5bdead4c66c026013485bd
Ignacio <progremb@gmail.com>
2024-11-09 18:48:05 -0500

    Archivos proyecto

---
+++ insertionSort.py
@@ -0,0 +1,10 @@
+def insertionShort(arr):
+    for j in range(1, len(arr)):
+        actual = arr[j]
+        i = j - 1
+
+        while i >= 0 and arr[i].numero_documento > actual.numero_documento:
+            arr[i + 1] = arr[i]
+            i -= 1
+        arr[i + 1] = actual
+    return arr

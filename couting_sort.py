69d01eb8629df1a1b8cdc64505f5670838a34c3e
Andres_1316 <yaiirandr3s.16@gmail.com>
2024-11-11 10:33:39 -0500

    Create couting_sort.py

---
+++ couting_sort.py
@@ -0,0 +1,19 @@
+def counting_sort(personas):
+    max_documento = max(persona.numero_documento for persona in personas)
+    min_documento = min(persona.numero_documento for persona in personas)
+    rango = max_documento - min_documento + 1
+    count = [0] * rango
+    output = [None] * len(personas)
+    
+    for persona in personas:
+        count[persona.numero_documento - min_documento] += 1
+        
+    for i in range(1, len(count)):
+        count[i] += count[i - 1]
+
+    for persona in reversed(personas):
+        index = count[persona.numero_documento - min_documento] - 1
+        output[index] = persona
+        count[persona.numero_documento - min_documento] -= 1
+
+    return output
